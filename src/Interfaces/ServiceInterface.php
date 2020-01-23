ab53811ae949164a05cb31cdf05d102670a6ee4e
eike-grundke <grundke.eike@gmail.com>
2020-01-23 17:37:41 +0100

    ServiceInterface -> getList : Object

--- src/Interfaces/ServiceInterface.php
+++ src/Interfaces/ServiceInterface.php
@@ -7,7 +7,7 @@ use LexofficeSdk\Interfaces\EntityInterface;
 interface ServiceInterface
 {
     public function get(string $id, array $query): EntityInterface;
-    public function getList(array $query);
+    public function getList(array $query): Object;
     public function create(EntityInterface $entity): Object;
     public function update(EntityInterface $entity): Object;
 
